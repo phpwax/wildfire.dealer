17d52e3028ba49d795c5498ddd67327f5f46e5a9
charlesmarshall <cm56marshall@googlemail.com>
2012-02-17 09:15:49 +0000

    admin controller

---
+++ resources/app/controller/admin/AdminDealerController.php
@@ -0,0 +1,9 @@
+<?php
+class AdminDealerController extends AdminComponent{
+  public $module_name = "dealer";
+  public $model_class = 'Dealer';
+  public $display_name = "Dealers";
+  public $dashboard = false;
+  public $tree_layout = false;
+}
+?>
