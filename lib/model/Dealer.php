8c336593939ee8020ff75c5c2a6d5f638833e112
charlesmarshall <cm56marshall@googlemail.com>
2012-02-17 09:11:53 +0000

    dealer model

---
+++ lib/model/Dealer.php
@@ -0,0 +1,41 @@
+<?
+
+class Dealer extends VehicleBaseModel{
+
+
+  public function setup(){
+    $this->define("brand", "ForeignKey", array('target_model'=>'Brand', 'required'=>true) );
+    $this->define("client_id", "IntegerField", array('maxlength'=>255) );
+
+    parent::setup();
+
+    $this->define("address_line_1", "CharField", array('maxlength'=>255, 'required'=>true, 'group'=>'contact') );
+    $this->define("address_line_2", "CharField", array('maxlength'=>255, 'group'=>'contact') );
+    $this->define("address_line_3", "CharField", array('maxlength'=>255, 'group'=>'contact') );
+    $this->define("city", "CharField", array('maxlength'=>255, 'group'=>'contact') );
+    $this->define("county", "CharField", array('maxlength'=>255, 'group'=>'contact') );
+    $this->define("postal_code", "CharField", array('maxlength'=>255, 'group'=>'contact') );
+    $this->define("telephone", "CharField", array('maxlength'=>255, 'group'=>'contact') );
+    $this->define("fax", "CharField", array('maxlength'=>255, 'group'=>'contact') );
+    $this->define("email", "CharField", array('maxlength'=>255, 'group'=>'contact') );
+    $this->define("opening_times", "TextField", array('widget'=>"TinymceTextareaInput", 'group'=>'contact'));
+
+    $this->define("active", "BooleanField", array('group'=>'status'));
+    $this->define("sales", "BooleanField", array('maxlength'=>2,'group'=>'status') );
+    $this->define("service", "BooleanField", array('maxlength'=>2,'group'=>'status') );
+    $this->define("parts", "BooleanField", array('maxlength'=>2,'group'=>'status') );
+    $this->define("specialism","CharField", array('group'=>'status'));
+
+    $this->define("lat", "CharField", array('editable'=>false));
+    $this->define("lng", "CharField", array('editable'=>false));
+    $this->define("website", "CharField", array('editable'=>false));
+    $this->define("api_status", "BooleanField", array('editable'=>false));
+
+  }
+
+
+
+
+}
+
+?>
