775e449d1ef8de14a9632d6e40a5b4956c9ea36c
charles marshall <charles@oneblackbear.com>
2012-03-17 10:57:42 +0000

    add in the dynamic lookup of the content model

--- setup.php
+++ setup.php
@@ -1,3 +1,9 @@
 <?
 CMSApplication::register_module("dealer", array("display_name"=>"Dealers", "link"=>"/admin/dealer/", 'split'=>true));
+
+if(!defined("CONTENT_MODEL")){
+  $con = new ApplicationController(false, false);
+  define("CONTENT_MODEL", $con->cms_content_class);
+}
+
 ?>
