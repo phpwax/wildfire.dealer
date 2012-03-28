6478d79e327b8f65ffff033d5b535182e0ec9ecb
charlesmarshall <cm56marshall@googlemail.com>
2012-03-28 18:33:40 +0100

    updated domain model

--- lib/model/Domain.php
+++ lib/model/Domain.php
@@ -4,7 +4,7 @@ class Domain extends WaxModel{
   public function setup(){
     $this->define("webaddress", "CharField", array('scaffold'=>true));
     $this->define("status", "BooleanField", array('label'=>'Live','scaffold'=>true));
-    $this->define("dealers", "ManyToManyField", array('target_model'=>'Dealer','scaffold'=>true));
+    $this->define("dealers", "ManyToManyField", array('target_model'=>'Dealer','scaffold'=>true, 'group'=>'relationships'));
   }
 
   public function before_save(){
