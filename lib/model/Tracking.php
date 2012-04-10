355234aca0cf76bdecda97b4b7384a8e1455670f
charlesmarshall <cm56marshall@googlemail.com>
2012-04-10 11:22:55 +0100

    make that dynamic

--- lib/model/Tracking.php
+++ lib/model/Tracking.php
@@ -4,7 +4,7 @@ class Tracking extends WaxModel{
   public function setup(){
     $this->define("source", "CharField", array('scaffold'=>true));
     $this->define("code", "TextField");
-    $this->define("pages", "ManyToManyField", array('target_model'=>'MGContent', 'group'=>'relationships'));
+    if(defined("CONTENT_MODEL")) $this->define("pages", "ManyToManyField", array('target_model'=>CONTENT_MODEL, 'group'=>'relationships'));
   }
 
   public function before_save(){
