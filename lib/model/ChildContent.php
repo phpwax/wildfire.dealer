a6128e85163fca59935c97b304647cef43665875
charlesmarshall <cm56marshall@googlemail.com>
2012-08-07 12:27:20 +0100

    fix the child call, was including itself

--- lib/model/ChildContent.php
+++ lib/model/ChildContent.php
@@ -16,11 +16,15 @@ class ChildContent extends HasManyField {
   //overwrite the lazy load to also fetch all of the content of the national_content as well
   public function lazy_load($target) {
     $data = parent::lazy_load($target);
-    if($this->model->row['dealer_content_id'] && ($national = $this->model->national_content) && ($kids = $national->children) && ($kids = $kids->filter("for_dealer", 1)->all()) ){
-      $kid_ids = array(0);
-      foreach($kids->rowset as $pg) $kid_ids[] = $pg['id'];
-      $ids = array_merge((array)$data->rowset, (array)$kid_ids);
+    if($this->model->row['dealer_content_id'] ){
+      $col = $this->model->get_col("national_content");
+      $name = $col->col_name;
+      $val = $this->model->{$col->col_name};
       $class = get_class($this->model);
+      $model = new $class($this->default_scope);
+      $kid_ids = array(0);
+      foreach($model->filter("id", $this->model->primval, "!=")->filter("parent_id", $val)->filter("for_dealer", 1)->all() as $pg) $kid_ids[] = $pg->primval;
+      $ids = array_merge((array)$data->rowset, (array)$kid_ids);
       $model = new $class("live");
       return $model->filter("id", $ids)->all();
     }
