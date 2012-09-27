280b11840048fb492f0c57859e5abd6ec81764af
Mike <michal.noskovic@googlemail.com>
2012-09-27 10:14:38 +0100

    primary on domain

--- lib/model/Domain.php
+++ lib/model/Domain.php
@@ -5,6 +5,7 @@ class Domain extends WaxModel{
     $this->define("webaddress", "CharField", array('scaffold'=>true));
     $this->define("status", "BooleanField", array('label'=>'Live','scaffold'=>true));
     if(constant("DEALER_MODEL")) $this->define("dealers", "ManyToManyField", array('target_model'=>DEALER_MODEL,'scaffold'=>true, 'group'=>'relationships'));
+    $this->define("is_primary", "BooleanField", array('scaffold'=>true));
   }
 
   public function before_save(){
