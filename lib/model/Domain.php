768dfaeb3699f93bd0778b860b36c3033b53aa0e
mikenosko <michal@oneblackbear.com>
2017-07-31 14:33:19 +0100

    support for primary domains

--- lib/model/Domain.php
+++ lib/model/Domain.php
@@ -5,6 +5,8 @@ class Domain extends WaxModel{
     $this->define("webaddress", "CharField", array('scaffold'=>true));
     $this->define("status", "BooleanField", array('label'=>'Live','scaffold'=>true));
     $this->define("dealers", "ManyToManyField", array('target_model'=>'Dealer','scaffold'=>true, 'group'=>'relationships'));
+    $this->define("is_primary", "BooleanField", array('scaffold'=>true));
+    parent::setup();
   }
 
   public function before_save(){
